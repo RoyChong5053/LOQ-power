3af4d86e8278ffee38015634090dd2617eb5c150
RoyChong5053 <hbctantra@gmail.com>
2026-09-10 07:42:33 +0800

    fix: add missing LOQPowerApp import in main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 import gi
 gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk, Gdk
+from gui.app import LOQPowerApp
 
 # Custom CSS for our widgets
 CUSTOM_CSS = """
